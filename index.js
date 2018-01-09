4fc0b865c8fb8c86578cf8776f07a121c01d6e7a
Ashley Marku <ashley.marku@lc.cuny.edu>
2018-01-09 15:45:27 -0500

    all da files

---
+++ index.js
@@ -0,0 +1,9 @@
+function clickMe(){
+	var list=document.getElementsByTagName("p");
+
+	list[0].style.backgroundColor="green";
+	list[1].style.backgroundColor="pink";
+	list[2].style.backgroundColor="red";
+	list[3].style.backgroundColor="blue";
+
+}
