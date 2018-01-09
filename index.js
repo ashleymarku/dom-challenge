ddf11f19d04454e683bcdec71a4fc49fce1f13fe
Ashley Marku <ashley.marku@lc.cuny.edu>
2018-01-09 16:38:27 -0500

    both part 1 and part 2

--- index.js
+++ index.js
@@ -7,3 +7,29 @@ function clickMe(){
 	list[3].style.backgroundColor="blue";
 
 }
+
+
+var num = 12345678
+
+function grab() {
+	
+	var user = document.forms["sup"]["userName"].value;
+	var email1 = document.forms["sup"]["email"].value;
+	var pass = document.forms["sup"]["password"].value;
+
+	
+
+	if(pass== num){
+		
+
+		alert("congrats you logged in!!");
+		document.getElementById("H").innerHTML = "PEANUTS!";
+		console.log(document.getElementById("H"));
+
+
+
+	}
+	else{
+		alert("Incorrect password! Try again");
+	}
+}
